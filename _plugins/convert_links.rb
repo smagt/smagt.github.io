3438788b714b82a9fad04f1d58ed2c54b735afa5
Patrick van der Smagt <smagt@argmax.ai>
2024-07-08 20:56:16 +0200

    cln

--- _plugins/convert_links.rb
+++
@@ -1,8 +0,0 @@
-# _plugins/convert_links.rb
-Jekyll::Hooks.register :posts, :pre_render do |post|
-  post.content = post.content.gsub(/\[([^\]]+)\]\(([^)]+)\.md\)/, '[\1](\2.html)')
-end
-
-Jekyll::Hooks.register :pages, :pre_render do |page|
-  page.content = page.content.gsub(/\[([^\]]+)\]\(([^)]+)\.md\)/, '[\1](\2.html)')
-end
